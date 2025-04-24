29499e6476c2b211bfe65b3776857500720b745a
mdangga <108461444+mdangga@users.noreply.github.com>
2025-04-24 19:47:26 +0800

    update

--- navbar.php
+++ navbar.php
@@ -1,14 +1,8 @@
-<!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-<!-- Sidebar Toggle (Topbar) -->
 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
     <i class="fa fa-bars"></i>
 </button>
-
-<!-- Topbar Navbar -->
 <ul class="navbar-nav ml-auto">
-    <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -18,14 +12,9 @@
                 ?>
             </span>
             <i class="fas fa-user-circle fa-2x text-gray-600"></i>
-        </a>
-        <!-- Dropdown - User Information -->
+        </a>>
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown">
-            <!-- <a class="dropdown-item" href="../update_profile.php" data-toggle="modal" data-target="#UpdateModal">
-                <i class="fas fa-user-edit mr-2 text-gray-400"></i>
-                Update Profile
-            </a> -->
             <a class="dropdown-item" href="../logout.php" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
@@ -33,6 +22,4 @@
         </div>
     </li>
 </ul>
-
 </nav>
-<!-- End of Topbar -->
